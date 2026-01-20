03e02fcf88f7ced43340584b7c21ab3e69d0c993
이규성 <rbtjd200@gmail.com>
2026-01-20 17:45:34 +0900

    feat:CreateRoomTab
    S14P11E205-77

--- codin_nator/frontend/src/pages/Home/tabs/CreateRoomTab.tsx
+++ codin_nator/frontend/src/pages/Home/tabs/CreateRoomTab.tsx
@@ -5,27 +5,37 @@ export default function CreateRoomTab() {
   return (
     <main className="flex-1 h-full flex flex-col bg-[#EEF4FA] p-6 overflow-y-auto relative">
       {/* 1. 전체 컨테이너 */}
-      <div className="max-w-5xl mx-auto w-full mb-8">
+      <div className=" w-full mb-6 ">
         {/* max-w-5xl =  “최대 너비 제한   | mx-auto =  margin-left/right를 auto 가로 가운데 정렬 */}
-        {/* 2. 페이지 내부헤더 */}
-        <h1 className="text-2xl font-bold text-gray-900 mb-2">
-          새 프로젝트 방 만들기
-        </h1>
-        <p className="text-gray-500">
-          Git 레파지토리를 연결하여 협업 코딩 환경을 구성합니다.
-        </p>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-0">
+          {/* 2. 페이지 내부헤더 */}
+          <div className="lg:col-span-2 flex flex-col justify-center pl-3">
+            <h1 className="text-2xl font-bold text-gray-900 mb-2">
+              새 프로젝트 방 만들기
+            </h1>
+            <p className="text-gray-500">
+              Git 레파지토리를 연결하여 협업 코딩 환경을 구성합니다.
+            </p>
+          </div>
+          <div className="lg:col-span-1 flex flex-col justify-center">
+            <button className="w-full h-[56px] bg-white text-lg font-medium text-gray-900 border border-gray-200 rounded-xl hover:bg-gray-50 transition-all shadow-sm">
+              방 생성하기
+            </button>
+          </div>
+        </div>
       </div>
 
       {/* 2. 레이아웃 나누기 (왼쪽:오른쪽 = 2:1) */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 pb-24">
         {/* [왼쪽] 입력 폼 영역 */}
-        <div className="lg:col-span-2 space-y-6">
+        <div className="lg:col-span-2 space-y-6 ">
           <FormCard
             title="프로젝트 정보"
             description="협업 방의 기본 정보를 입력해주세요."
             icon={
               <img src="/icons/folder-git.png" alt="📂" className="w-5 h-5" />
             }
+            className="fles-1 h-full" // 높이 꽉 채우기요
           >
             <FormInput
               label="방 제목"
@@ -56,8 +66,8 @@ export default function CreateRoomTab() {
           </FormCard>{" "}
           {/* 끝 */}
         </div>
-
-        <div className="space-y-6">
+        {/* 여기서 부터 오른쪽임 */}
+        <div className="space-y-6 lg:col-span-1 h-full flex flex-col gap-6">
           <FormCard
             title="Git 토큰 발급 방법"
             icon={<img src="/icons/key.png" alt="열쇠" className="w-5 h-5" />}
@@ -86,6 +96,7 @@ export default function CreateRoomTab() {
             icon={
               <img src="/icons/git-branch.png" alt="git" className="w-5 h-5" />
             }
+            className="flex-1" // 이 카드도 높이 꽉 맞추기
           >
             <div className="text-sm space-y-3">
               <p className="text-gray-600">
