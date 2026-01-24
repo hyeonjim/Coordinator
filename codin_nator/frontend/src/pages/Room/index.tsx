94f642214a739bac18661ba7cb3f04201b5edbd6
naehyun kim <scott2535@naver.com>
2026-01-24 14:34:20 +0900

    feat: add virtual terminal UI using xterm

--- codin_nator/frontend/src/pages/Room/index.tsx
+++ codin_nator/frontend/src/pages/Room/index.tsx
@@ -1,5 +1,5 @@
 import FileViewer from "../../components/room/file-viewer";
-
+import RoomTerminal from "../../components/room/room-terminal";
 export default function RoomPage() {
   return (
     <div className="h-screen">
@@ -14,7 +14,34 @@ export default function RoomPage() {
       {/* 메인 영역 */}
       <main className="border">
         <div>{/* 에디터 */}</div>
-        <div>{/* 터미널 */}</div>
+        <div>
+          {/* 터미널 */}
+          {/* 높이 지정(h-[500px]? 또는 h-screen)
+     일단 막 넣어놓음핑 */}
+          <div className="h-screen w-full">
+            <RoomTerminal
+              projectName="codin_nator"
+              branchName="main"
+              userName="User"
+              command="npm test"
+              output={`PASS  src/App.test.jsx
+              ✓ 화면에 Hello React가 보인다 (32 ms)
+
+              Test Suites: 1 passed, 1 total
+              Tests:       1 passed, 1 total
+              Snapshots:   0 total
+              Time:        1.214 s
+              Ran all test suites.`}
+              // 추가 상태바 설정핑
+              status={{
+                language: "Java",
+                encoding: "UTF-8",
+                connectedUsers: 2,
+                cursorInfo: "Ln 1, Col 1",
+              }}
+            />
+          </div>
+        </div>
       </main>
       {/* 오른쪽 사이드바 */}
       <aside className="border">
