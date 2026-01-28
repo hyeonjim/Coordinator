44da0344725331a8be6ae8a384c0d3180f74404c
김내현 <rlasogus1@gmail.com>
2026-01-28 14:29:15 +0900

    feat:add notfound.jpg

--- codin_nator/frontend/src/App.tsx
+++ codin_nator/frontend/src/App.tsx
@@ -6,21 +6,21 @@ import SettingPage from "./pages/Home/tabs/SettingTab";
 import RoomCreatePage from "./pages/Home/tabs/CreateRoomTab";
 import MyPage from "./pages/Home/tabs/MyPageTab";
 import HomeLayout from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-
         <Route path="/home" element={<HomeLayout />}>
           <Route index element={<MyPage />} />
           <Route path="mypage" element={<MyPage />} />
           <Route path="create" element={<RoomCreatePage />} />
           <Route path="settings" element={<SettingPage />} />
         </Route>
-
         <Route path="/room/:roomId" element={<RoomPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
