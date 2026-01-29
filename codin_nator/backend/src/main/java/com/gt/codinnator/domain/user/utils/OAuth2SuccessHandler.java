171d29a93350a3e98c48f03ee47620367208a19d
공윤정 <mulkwhale@naver.com>
2026-01-29 15:33:25 +0900

    feature: 방 생성, 로그인 시 git토큰 전달

--- codin_nator/backend/src/main/java/com/gt/codinnator/domain/user/utils/OAuth2SuccessHandler.java
+++ codin_nator/backend/src/main/java/com/gt/codinnator/domain/user/utils/OAuth2SuccessHandler.java
@@ -5,6 +5,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -17,6 +20,8 @@ import java.io.IOException;
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final JwtTokenProvider tokenProvider;
+    // GitHub 토큰을 가져오기 위한 서비스 주입
+    private final OAuth2AuthorizedClientService authorizedClientService;
 
     @Value("${app.frontend.url}")
     private String frontendUrl;
@@ -24,8 +29,16 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String gitId = String.valueOf(oAuth2User.getAttributes().get("id"));
+
+        // GitHub Access Token 추출
+        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(
+                oauthToken.getAuthorizedClientRegistrationId(),
+                oauthToken.getName()
+        );
+        String githubAccessToken = client.getAccessToken().getTokenValue();
 
         // JWT 토큰 생성
         String token = tokenProvider.createToken(gitId);
@@ -35,6 +48,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String targetUrl = UriComponentsBuilder.fromUriString(frontendUrl)
                 .path("/home")
                 .queryParam("token", token)
+                .queryParam("gitToken", githubAccessToken) // GitHub Access Token
                 .build().toUriString();
 
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
