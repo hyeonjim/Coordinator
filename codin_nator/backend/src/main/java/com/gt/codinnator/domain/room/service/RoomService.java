4c3c8dbc5c501d4841aee80ab5ee4f3413ad8b14
공윤정 <mulkwhale@naver.com>
2026-01-30 10:35:20 +0900

    fix: 로그인 후 방 생성 시 사용자 정보 담지 못하는 오류 수정

--- codin_nator/backend/src/main/java/com/gt/codinnator/domain/room/service/RoomService.java
+++ codin_nator/backend/src/main/java/com/gt/codinnator/domain/room/service/RoomService.java
@@ -27,7 +27,6 @@ public class RoomService {
         Room room = Room.builder()
                 .title(request.getName())
                 .userId(loginUserId) // 방장 ID 저장
-                .fileId(request.getFileId())
                 .gitToken(request.getGitToken())
                 .roomUrl(UUID.randomUUID().toString()) // 고유한 방 주소 생성
                 .build();
