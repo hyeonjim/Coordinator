f77628d5458d22e6701f4984a4c6245e243ae7ec
hyeonjim <sahacar2000@naver.com>
2026-02-04 00:02:20 +0900

    젠킨스 때문에 랜딩페이지 index 오류 코드 삭제

--- codin_nator/frontend/src/pages/Landing/index.tsx
+++ codin_nator/frontend/src/pages/Landing/index.tsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { useRef } from "react";
 import logo from "@/assets/images/logo_wh.png";
-import { getSocketBaseUrl } from "@/utils/socketUtils";
 
 // GitHub OAuth 인증 시작 URL (백엔드에서 처리)
 const GITHUB_OAUTH_URL = "/oauth2/authorization/github";
