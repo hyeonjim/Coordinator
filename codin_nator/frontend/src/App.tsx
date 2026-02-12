d7543a6952bfae926e8dd9d2cc781c9b42cfac55
김내현 <rlasogus1@gmail.com>
2026-02-12 17:59:31 +0900

    refactor:로딩페이지 추가

--- codin_nator/frontend/src/App.tsx
+++ codin_nator/frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { lazy, Suspense, useEffect } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 
 import PublicRoute from "./components/routes/PublicRoute";
 import ProtectedRoute from "./components/routes/ProtectedRoute";
 import { bootstrapAuth } from "./auth/bootstrapAuth";
+import LoadingPage from "./components/common/Loading";
 
 const LandingPage = lazy(() => import("./pages/Landing"));
 const RoomPage = lazy(() => import("./pages/Room"));
@@ -11,13 +12,24 @@ const HomeLayout = lazy(() => import("./pages/Home"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 
 export default function App() {
+  const [isInitializing, setIsInitializing] = useState(true);
+
   useEffect(() => {
-    bootstrapAuth();
+    const init = async () => {
+      await bootstrapAuth();
+      setIsInitializing(false);
+    };
+
+    init();
   }, []);
+
+  if (isInitializing) {
+    return <LoadingPage />;
+  }
 
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<LoadingPage />}>
         <Routes>
           <Route element={<PublicRoute />}>
             <Route path="/" element={<LandingPage />} />
